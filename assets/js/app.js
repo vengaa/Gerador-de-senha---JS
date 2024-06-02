7d71ba48be4faa40d71385377e3c58c69f7acfa3
Vengaa <luisfelipeadm.ps@gmail.com>
2024-06-01 21:10:55 -0300

    updates js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ let slideElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
-let containerPassword = document.querySelector("#container-password"); // Corrigido para #container-password
+let containerPassword = document.querySelector("#container-password"); 
 
 let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 let novaSenha = "";
